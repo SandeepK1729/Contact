16b938bb81b397c0175fe29737f8a43a27691714
SandeepK1729 <sandeepkumargalipelly@gmail.com>
2022-07-05 19:09:31 +0530

    1 file modified, 1 added

--- index.js
+++ index.js
@@ -2,7 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style.css';
 
-ReactDOM.render(
-  <h1>Hello, React!</h1>,
-  document.getElementById('root')
-);
+const user = 'Sandeep';
+const greet = <p>Hello, {user}!</p>;
+const root = document.getElementById('root');
+
+ReactDOM.render(<h1>Hello, React!</h1>, root);
+
+let counter = 0;
+
+function Message(props) {
+  return <p>{props.count}</p>;
+}
+
+function show() {
+  counter++;
+  ReactDOM.render(<Message count={counter} />, root);
+}
+setInterval(show, 1000);
